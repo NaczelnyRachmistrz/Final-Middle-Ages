ad247b1cd49cdd79368e8be30477d53a4cbe25fc
Mikołaj Walczak <mikiwalczak@gmail.com>
2016-05-05 18:53:17 +0200

    Map and Parse documentation

--- src/map.h
+++ src/map.h
@@ -1,3 +1,7 @@
+/**
+ * @file map.h
+ * @brief Hashtable of units
+ */
 #ifndef MIDDLE_AGES_MAP_H
 #define MIDDLE_AGES_MAP_H
 
@@ -5,14 +9,51 @@
 #include "unit.h"
 #include "unitlist.h"
 
+/**
+ * @brief Initializes map
+ * @detail Allocates memory for hashtable
+ * and sets size of the game map
+ * @param size size of the game map
+ */
 void mapInitialize(long size);
+
+/**
+ * Gets size of the game map
+ * @return returns size of the game map
+ */
 long mapGetSize();
+
+/**
+ * @brief Removes map
+ * @detail Frees memory of hashtable, removes all lists and units on the map
+ */
 void mapDestroy();
 
+/**
+ * @brief Prints fragment of the map to stdout
+ * @param from top left corner of the fragment
+ * @param to bottom right corner of the fragment
+ */
 void mapPrintFragment(Coordinates from, Coordinates to);
 
+/**
+ * Adds unit to the map
+ * @param unit unit to be added
+ */
 void mapAddUnit(Unit * unit);
+
+/**
+ * Gets unit from the map
+ * @param position coordinates to get unit from
+ * @return returns unit from position
+ */
 Unit * mapGetUnit(Coordinates position);
+
+/**
+ * @brief Removes unit from the map
+ * @detail Unit removed from map is not destroyed
+ * @param position coordinates to remove unit from
+ */
 void mapRemoveUnit(Coordinates position);
 
 #endif
--- src/parse.h
+++ src/parse.h
@@ -1,10 +1,31 @@
+/**
+ * @file parse.h
+ * @brief Loading and parsing input
+ */
 #ifndef MIDDLE_AGES_PARSE_H
 #define MIDDLE_AGES_PARSE_H
 
+/**
+ * @brief Number of available commands
+ * @see CommandType
+ */
 #define COMMANDS_COUNT 5
+
+/**
+ * Maximum length of correct command with parameters
+ */
 #define MAX_COMMAND_LENGTH 100
+
+/**
+ * Maximum arguments in correct command
+ * @see Command
+ */
 #define MAX_ARGUMENTS_COUNT 7
 
+/**
+ * Available commands types
+ * @see Command
+ */
 enum CommandType {
     INIT,
     MOVE,
@@ -14,12 +35,28 @@ enum CommandType {
     INVALID_INPUT
 };
 
+/**
+ * Command containing type and arguments
+ * @see getCommandFromInput
+ * @see removeCommand
+ */
 typedef struct {
     enum CommandType type;
     long arguments[MAX_ARGUMENTS_COUNT];
 } Command;
 
+/**
+ * @brief Reads and parses line from stdin
+ * @detail Reads characters from stdin up to '\n'
+ * lines longer than MAX_COMMAND_LENGTH are truncated and INVALID_COMMAND is returned
+ * @return Pointer to parsed command
+ */
 Command * getCommandFromInput();
+
+/**
+ * Destructor for command
+ * @param command command to be removed
+ */
 void removeCommand(Command * command);
 
 #endif
